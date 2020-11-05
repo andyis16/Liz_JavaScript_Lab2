3f185495d80c719a6519d645da719facbbe335fb
liz <elizabeth.collacott2fcagroup.com>
2020-11-04 21:57:41 -0500

    Lab2

---
+++ script.js
@@ -0,0 +1,45 @@
+console.log("Hello")
+
+//const randomDamage = () => Math.random()*11;
+
+let name = "Mitch Cuckovich";
+let age = 25;
+let birthday = "January 8, 1996";
+let detroitGC = true;
+let lifeEvents = ["I was born in Troy, Michigan", "I went to Hope College", "I went to Junior Oympics when I was 10 years old. I placed 14th in the nation in the 800 meter event.", "I'm a graduate of front-end bootcamp"];
+
+if (detroitGC = true) {
+    console.log ("My name is ",name, " and I am a student at Grand Circus in Detroit, Michigan.  I am currently ",age, "years old and my birthday is on ",birthday);
+}
+else {
+    console.log ("My name is ",name, " and I am a student at Grand Circus in Grand Rapids, Michigan.  I am currently ",age," years old and my birthday is on ",birthday);
+}
+
+var i;
+for (i=0; i < lifeEvents.length; i++) {
+    console.log (lifeEvents [i]);
+}
+
+let counter = 0;
+
+//randomNumber = Math.random()*11;
+//console.log (randomNumber);
+ 
+
+
+
+    while (true) {
+        let randomNumber = Math.floor(Math.random() * 11);
+
+        if (randomNumber !== 5)
+             {
+            console.log(randomNumber + " !==5");
+            counter++;
+        } 
+        else {
+            counter++;
+            console.log("5 === 5. It took " + counter + " iterations to randomly generate the number 5.");
+            break;
+        
+        }
+        }
